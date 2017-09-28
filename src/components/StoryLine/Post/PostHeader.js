e6dbb4cc061d412e0bdbbf0c9e0b8954c8dc405d
Vyacheslav Kamenev <ikamenev.slava@gmail.com>
2017-09-28 16:41:55 +0300

    fix: hoverable/clickable areas on main page

--- src/components/StoryLine/Post/PostHeader.js
+++ src/components/StoryLine/Post/PostHeader.js
@@ -1,6 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 import {Link} from 'react-router';
 import {connect} from 'react-redux';
+import {Tooltip, OverlayTrigger, ButtonToolbar, DropdownButton} from 'react-bootstrap';
 import {setVisibilityStory, deleteStory, pinStory} from '../../../redux/modules/story';
 import DeleteStory from '../../Popup/DeleteStory';
 import PinStory from '../../Popup/PinStory';
@@ -122,73 +123,73 @@ class PostHeader extends Component {
 
             {this.delimiterRender()}
             <div className="post-details-visibility">
-              <div className="post-details-visibility-icon">
-                <span className={this.chooseVisibilityIcon(visibility.value)}/>
-              </div>
               <div className="post-details-visibility-menu">
                 {/* <i className="caret"></i> */}
-                <ButtonToolbar>
-                  <DropdownButton className="profileMenu-btn" title={''} id={3} pullRight>
-                    <div className="sbox-visibility">
-                      <ul>
-                        <li>
-                          <div onClick={() => this.setVisibility(0, id)}>
-                            <input
-                              type="checkbox" name="public_visibility_story" id="public_visibility_story"
-                              checked={visibility.value === 0}/> {/*not defaultChecked*/}
-                            <label htmlFor={'public_visibility_story'}><span/></label>
-                            <div>
-                              <i className="public_icon"/>
-                              <p>Public</p>
-                            </div>
-                          </div>
-                        </li>
-                        <li>
-                          <div onClick={() => this.setVisibility(1, id)}>
-                            <input
-                              type="checkbox" name="private_visibility_story" id="private_visibility_story"
-                              checked={visibility.value === 1}/>
-                            <label htmlFor={'private_visibility_story'}><span/></label>
-                            <div>
-                              <i className="private_icon"/>
-                              <p>Private</p>
-                            </div>
-                          </div>
-                        </li>
-                        <li>
-                          <div onClick={() => this.setVisibility(2, id)}>
-                            <input
-                              type="checkbox" name="custom_visibility_story" id="custom_visibility_story"
-                              checked={visibility.value === 2}/>
-                            <label htmlFor={'custom_visibility_story'}><span/></label>
-                            <div>
-                              <i className="custom_icon"/>
-                              <p>Custom</p>
-                            </div>
-                          </div>
-                        </li>
-                        <li>
+                <DropdownButton
+                  className="profileMenu-btn"
+                  title={<span className={this.chooseVisibilityIcon(visibility.value)}/>}
+                  id={3}
+                  pullRight
+                >
+                  <div className="sbox-visibility">
+                    <ul>
+                      <li>
+                        <div onClick={() => this.setVisibility(0, id)}>
+                          <input
+                            type="checkbox" name="public_visibility_story" id="public_visibility_story"
+                            checked={visibility.value === 0}/> {/*not defaultChecked*/}
+                          <label htmlFor={'public_visibility_story'}><span/></label>
                           <div>
-                            <input
-                              type="checkbox" name="reset_visibility_story" id="reset_visibility_story"
-                              checked={false} onChange={this.handleCheckVisibility}/>
-                            <label htmlFor={'reset_visibility_story'}><span/></label>
-                            <div>
-                              <i className="reset_icon"/>
-                              <p>Reset as per visibility of books</p>
-                            </div>
+                            <i className="public_icon"/>
+                            <p>Public</p>
                           </div>
-                        </li>
-                        <span className="divider"/>
-                        <li>
+                        </div>
+                      </li>
+                      <li>
+                        <div onClick={() => this.setVisibility(1, id)}>
+                          <input
+                            type="checkbox" name="private_visibility_story" id="private_visibility_story"
+                            checked={visibility.value === 1}/>
+                          <label htmlFor={'private_visibility_story'}><span/></label>
                           <div>
-                            <p>History of Story Visibility</p>
+                            <i className="private_icon"/>
+                            <p>Private</p>
                           </div>
-                        </li>
-                      </ul>
-                    </div>
-                  </DropdownButton>
-                </ButtonToolbar>
+                        </div>
+                      </li>
+                      <li>
+                        <div onClick={() => this.setVisibility(2, id)}>
+                          <input
+                            type="checkbox" name="custom_visibility_story" id="custom_visibility_story"
+                            checked={visibility.value === 2}/>
+                          <label htmlFor={'custom_visibility_story'}><span/></label>
+                          <div>
+                            <i className="custom_icon"/>
+                            <p>Custom</p>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <div>
+                          <input
+                            type="checkbox" name="reset_visibility_story" id="reset_visibility_story"
+                            checked={false} onChange={this.handleCheckVisibility}/>
+                          <label htmlFor={'reset_visibility_story'}><span/></label>
+                          <div>
+                            <i className="reset_icon"/>
+                            <p>Reset as per visibility of books</p>
+                          </div>
+                        </div>
+                      </li>
+                      <span className="divider"/>
+                      <li>
+                        <div>
+                          <p>History of Story Visibility</p>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </DropdownButton>
               </div>
             </div>
 
