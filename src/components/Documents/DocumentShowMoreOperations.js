6343fc75320fb09fc00568a4dac76418ff6b467e
Vyacheslav Kamenev <ikamenev.slava@gmail.com>
2017-10-18 12:42:42 +0300

    update: show more operations on user documents page

--- src/components/Documents/DocumentShowMoreOperations.js
+++ src/components/Documents/DocumentShowMoreOperations.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import {NavDropdown, MenuItem} from 'react-bootstrap';
 import './show-more-operations.scss';
 
 class DocumentShowMoreOperations extends Component {
@@ -9,8 +8,18 @@ class DocumentShowMoreOperations extends Component {
     return (
       <div className="document-dropdown-menu">
         <div className="document-dropdown-menu-container">
-          <div onClick={showModalHandler}>Rename document</div>
-          <div>Delete</div>
+          <div><span className="open-icon"/><span>Open</span></div>
+          <hr />
+          <div><span className="share-icon"/><span>Share</span></div>
+          <div><span className="move-to-icon"/><span>Move to ...</span></div>
+          <div><span className="make-copy-icon"/><span>Make a copy ...</span></div>
+          <div onClick={showModalHandler}><span className="rename-icon"/><span>Rename document</span></div>
+          <div><span className="download-icon"/><span>Download</span></div>
+          <hr />
+          <div><span className="verify-icon"/><span>Verify Signatures</span></div>
+          <div><span className="sign-icon"/><span>Sign</span></div>
+          <hr />
+          <div><span className="delete-icon"/><span>Delete</span></div>
         </div>
       </div>
     );
