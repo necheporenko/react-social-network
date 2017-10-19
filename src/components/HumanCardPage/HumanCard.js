a6f0596bfe8bad55419f42cd331d7771ed039e27
Vyacheslav Kamenev <ikamenev.slava@gmail.com>
2017-10-19 17:35:50 +0300

    refactoring human card component

--- src/components/HumanCardPage/HumanCard.js
+++ src/components/HumanCardPage/HumanCard.js
@@ -18,97 +18,14 @@ const borderColorDark = 'rgb(162, 162, 162)';
 })
 
 export default class HumanCard extends Component {
-  constructor(props) {
-    super(props);
-
-    const {requestedUser, draftHumanCard} = this.props;
-
-    this.state = {
-      fullName: '',
-      publicAddress: props.draftHumanCard ? props.draftHumanCard.public_address : '',
-    };
-
-    if (draftHumanCard) {
-      this.state.fullName = draftHumanCard.full_name;
-    } else if (requestedUser && requestedUser.first_name && requestedUser.last_name) {
-      this.state.fullName = `${requestedUser.first_name} ${requestedUser.last_name}`;
-    }
+  constructor() {
+    super();
     
-    this.changeFullName = this.changeFullName.bind(this);
-    this.changePublicAddress = this.changePublicAddress.bind(this);
     this.saveDraft = this.saveDraft.bind(this);
-    this.requestHumanCard = this.requestHumanCard.bind(this);
-    this.copyAddress = this.copyAddress.bind(this);
     this.fnHumanCard = this.fnHumanCard.bind(this);
     this.emptyDraftHumanCard = this.emptyDraftHumanCard.bind(this);
     this.humanCardRender = this.humanCardRender.bind(this);
-    this.draftHumanCard = this.draftHumanCard.bind(this);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.draftHumanCard && (nextProps.draftHumanCard.full_name !== this.state.fullName ||
-      nextProps.draftHumanCard.public_address !== this.state.public_address)) {
-      this.setState({
-        fullName: nextProps.draftHumanCard.full_name,
-        publicAddress: nextProps.draftHumanCard.public_address
-      });
-    } else {
-      this.setState({
-        fullName: `${nextProps.requestedUser.first_name} ${nextProps.requestedUser.last_name}`
-      });
-    }
-  }
-
-  copyAddress = () => {
-    const div = document.querySelector('.markdown-human-card div');
-    const getAddress = div.querySelector('p:nth-child(3)');
-    let range;
-    let select;
-
-    if (document.createRange) {
-      range = document.createRange();
-      range.selectNodeContents(getAddress);
-      select = window.getSelection();
-      select.removeAllRanges();
-      select.addRange(range);
-      document.execCommand('copy');
-      select.removeAllRanges();
-    } else {
-      range = document.body.createTextRange();
-      range.moveToElementText(getAddress);
-      range.select();
-      document.execCommand('copy');
-    }
-  };
-
-  copyName = () => {
-    const div = document.querySelector('.markdown-human-card div');
-    const getName = div.querySelector('h2');
-    let range;
-    let select;
-
-    if (document.createRange) {
-      range = document.createRange();
-      range.selectNodeContents(getName);
-      select = window.getSelection();
-      select.removeAllRanges();
-      select.addRange(range);
-      document.execCommand('copy');
-      select.removeAllRanges();
-    } else {
-      range = document.body.createTextRange();
-      range.moveToElementText(getName);
-      range.select();
-      document.execCommand('copy');
-    }
-  };
-
-  changeFullName(event) {
-    this.setState({fullName: event.target.value});
-  }
-
-  changePublicAddress(event) {
-    this.setState({publicAddress: event.target.value});
+    this.draftHumanCardRender = this.draftHumanCardRender.bind(this);
   }
 
   saveDraft() {
@@ -119,53 +36,6 @@ export default class HumanCard extends Component {
     } else {
       createDraftHumanCard(this.inputFullName.value, authorizedUser.id, this.inputPublicAddress.value);
     }
-  }
-
-  requestHumanCard(url) {
-    const myInit = {
-      method: 'GET',
-      headers: new Headers(),
-      mode: 'cors',
-      cache: 'default'
-    };
-
-    const result = fetch(url, myInit);
-    result.then(response => {
-      return response.text();
-    }).then((text) => {
-      if (text !== this.state.markdownText) {
-        return this.setState({markdownText: text});
-      }
-    }).catch((ex) => {
-      console.log('failed', ex);
-    });
-
-  // const div = document.querySelector('.markdown-human-card div');
-  // if (div) {
-  //   function w(linkHC) {
-  //     const h1 = div.querySelector('h1');
-  //     const p5 = div.querySelector('p:nth-child(5)');
-  //     const p8 = div.querySelector('p:nth-child(8)');
-
-  //     h1.addEventListener('click', linkHC);
-  //     p5.addEventListener('click', linkHC);
-  //     p8.addEventListener('click', linkHC);
-  //   }
-  //   setTimeout(() => w(this.linkHC), 1000);
-  // }
-
-  //     const XHR = ('onload' in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
-  //     const xhr = new XHR();
-  // // (2) запрос на другой домен :)
-  //     xhr.open('GET', url, true);
-  //     xhr.onload = function () {
-  //       return <div>Hello</div>;
-  //       // return this.responseText;
-  //     };
-  //     xhr.onerror = function () {
-  //       alert(`Ошибка ${this.status}`);
-  //     };
-  //     xhr.send();
   }
   
   fnHumanCard(e) {
@@ -228,18 +98,14 @@ export default class HumanCard extends Component {
 
     return (
       <div className="markdown-human-card">
-        {/*<Link to={`/${slug}/documents/human-card/${box.human_card.public_address}`} className="markdown-human-card">*/}
-        {this.requestHumanCard(humanCard.url)}
         <ReactMarkdown source={humanCard.markdown}/>
-        {/*</Link>*/}
       </div>
     );
   }
 
-  draftHumanCard() {
+  draftHumanCardRender() {
     const {draftHumanCard, authorizedUser, requestedUser, emptyDraftHumanCard} = this.props;
     const {first_name} = requestedUser;
-    const {publicAddress, fullName} = this.state;
 
     if (!draftHumanCard && !emptyDraftHumanCard) {
       return null;
@@ -247,28 +113,30 @@ export default class HumanCard extends Component {
 
     return (
       <div className="draft-human-card">
-        {/*<div className="help-human-card"><i/></div>*/}
         <h1 style={{marginBottom: 0}}>HUMAN CARD</h1>
-        {/*<hr/>*/}
         <p style={{marginTop: '5px', marginBottom: 0}}> 
-          {/*<strong>Public Address:</strong>*/}
           {requestedUser.slug !== authorizedUser.slug 
             ? <input
               type="text"
               placeholder={`${first_name}'s public address will be here`}
-              value={publicAddress}
+              defaultValue={draftHumanCard 
+                ? draftHumanCard.public_adress 
+                : ''
+              }
+              ref={el => this.inputPublicAddress = el}
               readOnly
             />
             : <input
               type="text"
               placeholder={`${first_name}'s public address will be here`}
-              onChange={this.changePublicAddress}
-              value={publicAddress}
+              defaultValue={draftHumanCard 
+                ? draftHumanCard.public_address 
+                : ''
+              }
               ref={el => this.inputPublicAddress = el}
             />
           }
           
-          {/*<div className="help-human-card"><i/></div>*/}
         </p>
         <p style={{fontSize: '12px', marginTop: '5px', marginBottom: '10px'}}>
           This public address has been established for:
@@ -277,24 +145,27 @@ export default class HumanCard extends Component {
           {requestedUser.slug !== authorizedUser.slug
             ? <input
               type="text" placeholder="Type name by which people know you"
-              value={fullName}
+              defaultValue={draftHumanCard 
+                ? draftHumanCard.full_name 
+                : `${requestedUser.first_name} ${requestedUser.last_name}`
+              }
+              ref={el => this.inputFullName = el}
               style={{fontSize: '20px'}}
               readOnly
             />
             : <input
               type="text" placeholder="Type name by which people know you"
-              onChange={this.changeFullName}
-              value={fullName}
+              defaultValue={draftHumanCard 
+                ? draftHumanCard.full_name 
+                : `${requestedUser.first_name} ${requestedUser.last_name}`
+              }
               ref={el => this.inputFullName = el}
               style={{fontSize: '20px'}}
             />
           }
-          {/*<div className="help-human-card"><i/></div>*/}
         </p>
         <p style={{color: '#d2d2d2', fontSize: '12px', magrinTop: 5, marginBottom: 10}}>
           Digital signature and signing date will be here.
-          {/*<span>  your signature will be here</span>*/}
-          {/*<div className="help-human-card"><i/></div>*/}
         </p>
       </div>
     );
@@ -305,25 +176,25 @@ export default class HumanCard extends Component {
     const {humanCard, authorizedUser, requestedUser, draftHumanCard, emptyDraftHumanCard} = this.props;
 
     return (
-      <div className="wrapper-human-card">
+      <div className="human-card-container">
         <div
-          className="human-card human-card-preview"
+          className="human-card"
           onClick={this.fnHumanCard}>
           {this.emptyDraftHumanCard()}
           {this.humanCardRender()}
-          {this.draftHumanCard()}
+          {this.draftHumanCardRender()}
         </div>
         {requestedUser.slug === authorizedUser.slug && (draftHumanCard || emptyDraftHumanCard) &&
           <div className="human-card-btn">
             <button className="btn-brand" onClick={this.saveDraft}>Save</button>
             <SignHumanCard
-              fullName={this.state.fullName}
-              publicAddress={this.state.publicAddress}
+              fullName={'' }
+              publicAddress={''}
             />
           </div>
         }
       </div>
-    ); 
+    );
   }
 }
 
